92971be6b27165dbdd3dc741bd584948b74fb2e4
Justin Krysinski <jkrysinski@icloud.com>
2025-06-08 10:56:29 -0500

    removed quit button

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -7,7 +7,3 @@ func _ready() -> void:
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
-
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
